c723a22d5ca172b138066281d9e64741fcf87e4b
claro <c_ppogado67@yahoo.com>
2026-09-25 10:55:58 +0800

    04

--- app/test_main.py
+++ app/test_main.py
@@ -6,17 +6,19 @@ from app.main import can_access_google_page
 
 
 @pytest.mark.parametrize(
-	("valid_url", "has_internet", "expected_result"),
-	[
-		(True, True, "Accessible"),
-		(False, True, "Not accessible"),
-		(True, False, "Not accessible"),
-	],
+    ("valid_url", "has_internet", "expected_result"),
+    [
+        (True, True, "Accessible"),
+        (False, True, "Not accessible"),
+        (True, False, "Not accessible"),
+    ],
 )
-def test_can_access_google_page(valid_url, has_internet, expected_result):
-	with patch("app.main.valid_google_url", return_value=valid_url), patch(
-		"app.main.has_internet_connection", return_value=has_internet
-	):
-		result = can_access_google_page("https://www.google.com")
+def test_can_access_google_page(
+    valid_url: bool, has_internet: bool, expected_result: str
+) -> None:
+    with patch("app.main.valid_google_url", return_value=valid_url), patch(
+        "app.main.has_internet_connection", return_value=has_internet
+    ):
+        result = can_access_google_page("https://www.google.com")
 
-	assert result == expected_result
+    assert result == expected_result
